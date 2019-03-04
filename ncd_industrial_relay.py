2878ff6bfb8d71d5389e28c9e10d3b7ad2bca6c2
teknix <teknix@users.noreply.github.com>
2019-03-04 14:48:53 -0800

    corrected turn off and on by index to reflect api

--- ncd_industrial_relay.py
+++ ncd_industrial_relay.py
@@ -17,13 +17,13 @@ class Relay_Controller:
 	def turn_on_relay_by_index(self, relay):
 		lsb = relay-1 & 255
 		msb = relay >> 8
-		command = self.wrap_in_api([254,48,lsb,msb])
+		command = self.wrap_in_api([254,148,lsb,msb])
 		return self.process_control_command_return(self.send_command(command, 4))
 	
 	def turn_off_relay_by_index(self, relay):
 		lsb = relay-1 & 255
 		msb = relay >> 8
-		command = self.wrap_in_api([254,47,lsb,msb])
+		command = self.wrap_in_api([254,147,lsb,msb])
 		return self.process_control_command_return(self.send_command(command, 4))
 	
 	def turn_off_relay_group(self, relay, bank, group_size):
@@ -119,7 +119,7 @@ class Relay_Controller:
 			self.combus.write(command)
 			return self.combus.read(bytes_back)
 		else:
-			self.combus.send(command)
+			self.combus.send(bytearray(command))
 			return self.combus.recv(bytes_back)
 		
 	def process_control_command_return(self, data):
